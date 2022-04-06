98e09e7ac474b3d25e74da94196d93eb926f70fd
Carina Beauvais <Beauvais.Carina@mayo.edu>
2022-04-06 16:07:25 -0500

    deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,4 +9,22 @@ MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg)
 #6 Determine p-value and r-squared value
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg))
+# Eliminate independent variables with little impact on mpg:
+lm(mpg ~ vehicle_length + ground_clearance, data=MechaCar_mpg)
+summary(lm(mpg ~ vehicle_length + ground_clearance, data=MechaCar_mpg))
 
+## Deliverable 2
+#2 import and read csv file
+mecha_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+#3 Create total summary dataframe to get mean, median, variance and sd
+total_summary <- mecha_coil %>% summarize(Mean_PSI=mean(PSI),
+                                          Median_PSI=median(PSI),
+                                          Var_PSI=var(PSI),
+                                          Std_Dev_PSI=sd(PSI),
+                                          Num_Coil=n(), .groups = 'keep') 
+#4 Create lot summary dataframe using group_by and summarize
+lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),
+                                                                         Median_PSI=median(PSI),
+                                                                         Var_PSI=var(PSI),
+                                                                         Std_Dev_PSI=sd(PSI),
+                                                                         Num_Coil=n(), .groups = 'keep')
